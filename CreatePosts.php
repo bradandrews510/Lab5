40832ce374ab1182ad8e46997bf912c9f6ab3239
bradandrews510 <42749434+bradandrews510@users.noreply.github.com>
2018-10-28 22:46:19 -0500

    Updated Lab5
    Added Exercise 3 and 4.

---
+++ CreatePosts.php
@@ -0,0 +1,40 @@
+<?php
+$user_name = $_POST["user_name"];
+$user_content = $_POST["user_content"];
+$mysqli = new mysqli("mysql.eecs.ku.edu", "b142a678", "Taer4eiN", "b142a678");
+
+if ($mysqli->connect_errno) 
+{
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+else
+{
+	$query = "SELECT * FROM Users WHERE user_id='$user_name';";
+	if (empty($user_content))
+	{
+		echo "No content in post!";
+	}
+	else 
+	{
+		$query = "SELECT * FROM Users WHERE user_id='$user_name';";
+		if ($entity = $mysqli->query($query)) 
+		{
+   			if($row = $entity->fetch_assoc()) 
+			{
+      				$query = "INSERT INTO Posts (content,author_id) VALUES ('$user_content','$user_name');";
+        			if($mysqli->query($query))
+				{
+          				echo "Post Added!";
+        			}
+    			}
+    			else
+			{
+      				echo "User not found!";
+    			}
+    			$entity->free();
+		}
+	}
+}
+$mysqli->close();
+?>
